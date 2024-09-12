6cb6189c0ad5124a0a1df1e69a85712a1a7edb22
JeromeJinzh <jinzihan.jin@mail.utoronto.ca>
2024-09-13 05:50:18 +0800

    add method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -8,11 +8,11 @@ class FizzBuzz {
         int i = 1;
         while (i < 100) {
 
-            i = DoFizzBuzz(i);
+            i = doFizzBuzz(i);
         }
     }
 
-    private static int DoFizzBuzz(int i) {
+    private static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
